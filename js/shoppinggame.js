59cdbb077a8a3b5e538582b780d7766f70f3ca12
AlainNgauthier <ndamweyalaingauthier@ymail.com>
2021-10-04 12:00:41 -0300

    Added Module 3: Loading Master Data

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -83,6 +83,7 @@ const loadProducts = (map, prodId) => {
     try {
         // Call Object.keys() to load the property names of the Product object in to prodKeys array here
         let prodKeys = [];
+        prodKeys = Object.keys(Product);
 
         let iterator_obj = map.entries();
 
@@ -93,6 +94,7 @@ const loadProducts = (map, prodId) => {
 
                 // Create and assign an instance of Product to prodObj here
                 let prodObj;
+                prodObj = new Product();
 
                 if (prodObj != undefined && prodObj != null) {
                     for (let i = 0; i < prodKeys.length; i++) {
@@ -127,6 +129,7 @@ const loadMagicProducts = (map, prodId) => {
     try {
         // Call Object.key() to load the property names of the MagicProduct object in to magProdKeys array here
         let magProdKeys = [];
+        magProdKeys = Object.keys(MagicProduct);
 
         let iterator_obj = map.entries();
 
@@ -137,7 +140,8 @@ const loadMagicProducts = (map, prodId) => {
 
                 // Create and assign an instance of MagicProduct to prodObj here
                 let magProdObj;
-
+                magProdObj = new MagicProduct();
+                
                 if (magProdObj != undefined && magProdObj != null) {
                     for (let i = 0; i < magProdKeys.length; i++) {
                         let property = magProdKeys[i];
